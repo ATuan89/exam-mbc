31747f777654ebf157ff521994f1fab36503b56b
ATuan89 <tuanninh1290@gmail.com>
2025-07-29 16:34:22 +0700

    Update admin.py

--- myproject/home/admin.py
+++ myproject/home/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Department, Job
 
-# Register your models here.
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('department_code', 'department_name', 'valid')
+    list_filter = ('valid',)
+    search_fields = ('department_name',)
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('job_code', 'job_name', 'valid')
+    list_filter = ('valid',)
+    search_fields = ('job_name',)
